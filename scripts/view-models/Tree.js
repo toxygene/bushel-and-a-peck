d6166a8181557ee14da093c1865cfab7897f2f68
Justin Hendrickson <justin.hendrickson@gmail.com>
2014-07-25 21:54:23 -0500

    Update Tree.js

--- scripts/view-models/Tree.js
+++ scripts/view-models/Tree.js
@@ -12,14 +12,26 @@ define(function() {
         this.type = ko.observable(type);
         this.map = map;
 
+        this.onPopupOpenHandler = this.onPopupOpen.bind(this);
+        this.onPopupCloseHandler = this.onPopupClose.bind(this);
+
         this.marker = L.marker([this.latitude(), this.longitude()]);
-        this.marker.bindPopup($('#edit').get(0));
+        this.marker.bindPopup($('#popup').get(0));
+        this.marker.on('popupopen', this.onPopupOpenHandler);
+        this.marker.on('popupclose', this.onPopupCloseHandler);
         this.show();
     };
 
     Tree.prototype.hide = function() {
         this.map.removeLayer(this.marker);
         return this;
+    };
+    
+    Tree.prototype.onPopupClose = function(event) {
+    };
+    
+    Tree.prototype.onPopupOpen = function(event) {
+        ko.applyBindings(this, this.getPopup());
     };
 
     Tree.prototype.show = function() {
